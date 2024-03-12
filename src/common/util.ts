71377e16c3194aa1168637b587cab1f64c79215d
Sean Shirazi <sean@computing-den.com>
2024-03-12 20:47:50 +0300

    logo color

--- src/common/util.ts
+++ src/common/util.ts
@@ -11,4 +11,4 @@ export function escapeRegExp(str: string): string {
   return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-export const CACHE_VERSION = '28';
+export const CACHE_VERSION = '29';
