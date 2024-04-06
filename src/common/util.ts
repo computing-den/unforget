9adca719e7bdff29323008e3ac3fcbb4602ac194
Sean Shirazi <sean@computing-den.com>
2024-04-06 19:28:10 +0300

    updated add icon

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,6 +1,6 @@
 import type * as t from './types.js';
 
-export const CACHE_VERSION = 78;
+export const CACHE_VERSION = 79;
 
 export function isNoteNewerThan(a: t.NoteHead, b?: t.NoteHead): boolean {
   return (
