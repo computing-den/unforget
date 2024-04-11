e1156ea2b6a2790c2290f6fc7c922c4033e8ea2a
Sean Shirazi <sean@computing-den.com>
2024-04-11 21:50:45 +0300

    style

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,6 +1,6 @@
 import type * as t from './types.js';
 
-export const CACHE_VERSION = 140;
+export const CACHE_VERSION = 141;
 
 export function isNoteNewerThan(a: t.NoteHead, b?: t.NoteHead): boolean {
   return (
