0e9442f2aacf43d20b81ed4087eb0c2066288656
Sean Shirazi <sean@computing-den.com>
2024-05-23 12:15:43 +0300

    ...

--- src/server/index.ts
+++ src/server/index.ts
@@ -253,7 +253,7 @@ app.get(['/', '/import', '/export', '/about', '/archive', '/n/:noteId', '/login'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>unforget</title>
+    <title>Unforget</title>
     <link rel="stylesheet" href="/style.css">
     <link rel="manifest" href="/manifest.json" />
     <link rel="icon" href="/icon-256x256.png" type="image/png" />
